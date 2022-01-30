0e21ed9ab3cefb7d3509f2c0adabba06ca873e0e
Amir Zec <amirzecdev@outlook.com>
2022-01-30 17:44:58 +0100

    Edit component added and Dashboard get api

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,2 +1,27 @@
 <template>
+    <ol>
+    <li v-for="data in data">
+    {{ data.title }}
+    {{ data.firstname }}
+    {{ data.lastname}}
+    {{ data.jobtitle }}
+    <router-link :to="{name: 'edit', params: {id: data.id } }" tag="button">Edit</router-link>
+    </li>
+    </ol>
 </template>
+<script>
+export default {
+    data () {
+        return {
+            data: []
+        }
+
+    },
+    created() {
+        window.axios.get('api/employees').then(response => {
+            this.data = response.data.data
+
+        })
+    }
+}
+</script>
