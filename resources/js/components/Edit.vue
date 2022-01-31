ab2ebaf6e7bc6f9eb6e9965d48fd7de4b46b7531
Amir Zec <amirzecdev@outlook.com>
2022-01-31 14:32:49 +0100

    Update API implemented

--- resources/js/components/Edit.vue
+++ resources/js/components/Edit.vue
@@ -1,20 +1,43 @@
 <template>
-<form method="POST">
+
+<form>
   <label for="fname">Title</label>
   <input type="text" name="title" v-model="title"><br>
   <label for="lname">First name:</label>
   <input type="text"  name="firstname" v-model="firstname"><br>
   <label for="lastname">Last name:</label>
   <input type="text"  name="lastname" v-model="lastname"><br>
-   <label for="jobtitle">Job title:</label>
+  <label for="jobtitle">Job title:</label>
   <input type="text"  name="jobtitle" v-model="jobtitle"><br>
-  <button @click.stop.prevent="">Submit</button>
+  <button @click.stop.prevent="upd">Submit</button>
 </form>
+
 </template>
 <script>
 export default {
 data() {
-    
-} 
+  return {
+          title: null,
+          firstname: null,
+          lastname: null,
+          jobtitle: null,
+  }
+},
+created() {
+    window.axios.get('/api/employees/'+this.$route.params.id).then(response => {
+      this.title = response.data.data.title
+      this.firstname = response.data.data.firstname
+      this.lastname = response.data.data.lastname
+      this.jobtitle = response.data.data.jobtitle
+    })
+  }, methods: {
+    upd() {
+     var formData = {'title':this.title,'firstname':this.firstname,'lastname':this.lastname,'jobtitle':this.jobtitle}
+      window.axios.put('/api/employees/'+this.$route.params.id, formData ).then(response => {
+                    
+      })
+    }
+  }
+
 }
 </script>
