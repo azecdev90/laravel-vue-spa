103f372ce7fa1406d032fdc5364fe2e6cc57b5c8
Amir Zec <amirzecdev@outlook.com>
2022-02-07 21:12:45 +0100

    Last page text changed

--- public/js/resources_js_components_Dashboard_vue.js
+++ public/js/resources_js_components_Dashboard_vue.js
@@ -221,7 +221,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     onClick: _cache[0] || (_cache[0] = function ($event) {
       return $data.currentPage = $data.firstPage;
     })
-  }, "FirstPage"), ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($options.totalPage, function (n) {
+  }, "First page"), ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($options.totalPage, function (n) {
     return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("a", {
       key: n,
       onClick: function onClick($event) {
